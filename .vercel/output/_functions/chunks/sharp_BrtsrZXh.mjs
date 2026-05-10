1c73a9794c7201fbdb7cbee099931e3f920886aa
Anas Ballar <anasballar@gmail.com>
2026-05-10 17:18:47 +0200

    fix mobile nav layout to stack vertically

--- .vercel/output/_functions/chunks/sharp_BrtsrZXh.mjs
+++ .vercel/output/_functions/chunks/sharp_BrtsrZXh.mjs
@@ -1,5 +1,5 @@
-import { A as AstroError, U as UnsupportedImageFormat, o as MissingSharp } from './entrypoint_CHer_Szg.mjs';
-import { b as baseService, d as detector, p as parseQuality } from './generic_9A9w-jE1.mjs';
+import { A as AstroError, U as UnsupportedImageFormat, o as MissingSharp } from './entrypoint_Br1xkl25.mjs';
+import { b as baseService, d as detector, p as parseQuality } from './generic_BA_Tn-xQ.mjs';
 
 let sharp;
 const qualityTable = {
--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,1 +1,1 @@
-export { p as default } from './chunks/entrypoint_CHer_Szg.mjs';
+export { p as default } from './chunks/entrypoint_Br1xkl25.mjs';
